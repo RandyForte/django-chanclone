6be93bd41bed6413ce6524880b8d037cdfe0bfb1
RandyForte <forterandy@outlook.com>
2017-05-03 13:08:54 -0400

    Fixed database errors

--- chanClone/chan/admin.py
+++ chanClone/chan/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from chan.models import Post, Board, Thread
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Board)
+admin.site.register(Post)
 admin.site.register(Thread)
