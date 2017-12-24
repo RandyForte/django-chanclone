19edb2f16c7ef0c942dde4b04b1c6ddb88dafb4f
RandyForte <forterandy@outlook.com>
2017-12-24 12:15:40 -0500

    Added the static directory for serving static files

--- chanClone/chanClone/settings.py
+++ chanClone/chanClone/settings.py
@@ -14,11 +14,11 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 #STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 
